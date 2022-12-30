fce5626ed67da6dc8039dcfd8571a5a8b0cd180c
Tomi-422 <108313322+Tomi-422@users.noreply.github.com>
2022-12-29 23:14:50 -0300

    Subida desafio

---
+++ Server/app.js
@@ -0,0 +1,26 @@
+const express = require ('express')
+const app = express()
+const ProductManager = require ('../desafio3')
+const productManager = new ProductManager('./database/productos.JSON')
+const PORT = 8080
+
+
+app.get ('/products', async (req,res)=>{
+const products = await productManager.getProducts();
+const {limit} = req.query
+if (limit) return res.json(products.slice(0,limit))
+else return res.json(products)
+})
+
+app.get ('/products/:pid', async (req,res)=>{
+    const products = await productManager.getProducts();
+    const {pid} = req.params
+    const product = products.find (products => products.id === parseInt(pid))
+
+    if (product) return res.status(200).json(product)
+    else return res.status(404).json({message:'Product not found'});
+})
+
+app.listen(PORT, () => {
+console.log (`Server running on port ${PORT}`)
+})
